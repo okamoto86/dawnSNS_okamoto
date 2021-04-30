8d53060d64ecffc81cbeb4b4cb0548d2af378e4e
okamoto86 <on86.okamoto.kaho@gmail.com>
2021-04-30 19:45:25 +0900

    Userのシードファイルにレコードを初期登録。

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            ['username' => '田中'],
+            ['username' => '佐藤'],
+            ['username' => '新井']
+        ]);
+    }
+}
