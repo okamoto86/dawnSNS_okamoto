cc4e74df75495e61d1a8d724fc6c75e2dcb6f728
okamoto86 <on86.okamoto.kaho@gmail.com>
2021-06-02 15:57:44 +0900

    auth認証

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     //
     public function followList(){
         return view('follows.followList');
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     //
     public function profile(){
         return view('users.profile');
